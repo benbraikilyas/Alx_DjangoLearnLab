c66c0fb5947a56ea7392ae0a19153705d91caac9
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-02-20 15:06:41 +0100

    admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
 from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")  # عرض الأعمدة الرئيسية
+    list_filter = ("publication_year", "author")  # إضافة فلاتر للبحث السريع
+    search_fields = ("title", "author")  # تفعيل البحث باستخدام العنوان أو المؤلف
