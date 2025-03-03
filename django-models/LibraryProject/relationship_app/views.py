96ebe8105318332a5244910356ca2c453801efa8
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-03 19:55:08 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,7 @@
-from django.shortcuts import render
-from .models import Book
 from django.views.generic.detail import DetailView
-
-# عرض وظيفي لعرض جميع الكتب
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+from .models import Library  # ✅ تأكد من استيراد Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'  # ربط العرض بالقالب المناسب
-    context_object_name = 'library'  # تغيير اسم المتغير داخل القالب
-
+    template_name = "relationship_app/library_detail.html"  # ✅ تأكد من وجود القالب بهذا الاسم
+    context_object_name = "library"
