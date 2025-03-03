3bd2af01d16005a97b6d7d6ee93816cbbcfcdf4d
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-03 16:40:24 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,3 +6,15 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
 
+
+
+from django.views.generic import DetailView
+from .models import Library
+
+# عرض كائني لعرض تفاصيل مكتبة معينة
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'  # ربط العرض بالقالب المناسب
+    context_object_name = 'library'  # تغيير اسم المتغير داخل القالب
+
+
