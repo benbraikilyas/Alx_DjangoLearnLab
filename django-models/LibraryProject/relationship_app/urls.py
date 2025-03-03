3cd0d53415858d1a2d9790284aea853dfbeaf3bc
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-03 21:17:52 +0000

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,3 +5,16 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),  # العرض القائم على الدالة (Function-Based View)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # العرض القائم على الفئة (Class-Based View)
 ]
+
+
+
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),  # المسار للـ function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # المسار للـ class-based view
+]
+
+
+
