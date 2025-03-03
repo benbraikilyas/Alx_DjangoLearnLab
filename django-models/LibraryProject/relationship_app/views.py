9cb90cf1311b5581672d846bccb8426e46d6e03a
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-03 22:27:04 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book  # ✅ تأكد من استيراد Book
 from django.views.generic.detail import DetailView  # ✅ تأكد من وجود هذا الاستيراد
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     books = Book.objects.all()  # ✅ تأكد من استخدام الاستعلام الصحيح
@@ -51,4 +51,29 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# تحقق مما إذا كان المستخدم "Admin"
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# تحقق مما إذا كان المستخدم "Librarian"
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# تحقق مما إذا كان المستخدم "Member"
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
