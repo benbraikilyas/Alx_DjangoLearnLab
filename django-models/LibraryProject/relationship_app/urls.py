be000125582bec7b0dd06ea57672544092bd7a7e
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-03 21:55:40 +0000

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView  # تأكد من استيراد العروض (views)
 from .views import login_view, logout_view, register_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # العرض القائم على الدالة (Function-Based View)
@@ -23,4 +24,12 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
+]
+
+
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
