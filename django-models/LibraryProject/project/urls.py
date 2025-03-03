2d420c458d404ba28ff53d707b7e1dcdc9ee8135
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-03 16:42:12 +0000

    Update urls.py

--- django-models/LibraryProject/project/urls.py
+++ django-models/LibraryProject/project/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),  # رابط لعرض الكتب
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # رابط لعرض مكتبة معينة
+]
+
+
+
