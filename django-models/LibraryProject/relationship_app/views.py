2390c8a80e8a62b1d21e51a7f86c002148641132
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-03 19:56:13 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,4 @@
-from django.views.generic.detail import DetailView
-from .models import Library  # ✅ تأكد من استيراد Library
-
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"  # ✅ تأكد من وجود القالب بهذا الاسم
+    template_name = "relationship_app/library_detail.html"  # ✅ تأكد من تحديد القالب بشكل صحيح
     context_object_name = "library"
