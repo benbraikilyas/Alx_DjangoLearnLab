3f382d7675f4d984d0e7942d4b698431f001a9f8
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-10 03:07:49 +0000

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,7 @@ from .views import BookList  # تأكد من استيراد الـ View الصح
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token  
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
@@ -18,4 +19,7 @@ urlpatterns = [
 
     # Include the router URLs for the BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
+]
+
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
