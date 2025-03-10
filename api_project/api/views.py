b936e9074a68a122d4e8051d272d1a5099301d75
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-10 02:57:20 +0000

    Delete api_project directory

--- api_project/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import book
-from .serializers import BookSerializer
-
-class BookList(generics.ListAPIView):
-    queryset = book.objects.all()  
-    serializer_class = BookSerializer
