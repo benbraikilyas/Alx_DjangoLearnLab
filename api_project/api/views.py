80541d13d434099549acd922e73c3b44086b9d9b
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-10 03:10:18 +0000

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated 
 
 class BookList(generics.ListAPIView):
     queryset = book.objects.all()  
@@ -11,3 +12,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # جلب جميع الكتب من قاعدة البيانات
     serializer_class = BookSerializer  # استخدام BookSerializer لتحويل البيانات
+    permission_classes = [IsAuthenticated]  # السماح فقط للمستخدمين المفعلين بالوصول
