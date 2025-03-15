ede38d7919700b77a436ca00e8e1aeef09173350
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-15 04:52:19 +0000

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,22 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import (
-    BookListView,
-    BookDetailView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView
-)
-
-urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
-]
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    path('api/', include('api.urls')),  
 ]
