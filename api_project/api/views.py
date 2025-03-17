57f64ea37d6de0943ef3c8d765f802c67e322545
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-17 00:48:46 +0000

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,26 +1,15 @@
-from rest_framework import generics
-from .models import book
+from rest_framework import generics, viewsets, filters as drf_filters
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+import django_filters
+from .models import Book
 from .serializers import BookSerializer
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated 
-from django_filters import rest_framework 
-from rest_framework import viewsets, filters as drf_filters
 
 
-class BookList(generics.ListAPIView):
-    queryset = book.objects.all()  
-    serializer_class = BookSerializer
-
-# إنشاء ViewSet للتعامل مع عمليات CRUD
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()  
-    serializer_class = BookSerializer  
-    permission_classes = [IsAuthenticated] 
-
-class BookFilter(filters.FilterSet):
-    title = filters.CharFilter(lookup_expr='icontains') 
-    author = filters.CharFilter(field_name='author__name', lookup_expr='icontains') 
-    publication_year = filters.NumberFilter() 
+class BookFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains') 
+    author = django_filters.CharFilter(field_name='author__name', lookup_expr='icontains') 
+    publication_year = django_filters.NumberFilter() 
 
     class Meta:
         model = Book
@@ -30,30 +19,12 @@ class BookFilter(filters.FilterSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
     filter_backends = [
-        filters.DjangoFilterBackend,  
+        DjangoFilterBackend,  
         drf_filters.SearchFilter,     
         drf_filters.OrderingFilter    
     ]
     filterset_class = BookFilter
-
-
-class BookFilter(filters.FilterSet):
-    title = filters.CharFilter(lookup_expr='icontains') 
-    author = filters.CharFilter(field_name='author__name', lookup_expr='icontains') 
-    publication_year = filters.NumberFilter() 
-
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'publication_year']
-
-
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    filter_backends = [
-        filters.DjangoFilterBackend,  
-        drf_filters.SearchFilter,     
-        drf_filters.OrderingFilter    
-    ]
-    filterset_class = BookFilter
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
