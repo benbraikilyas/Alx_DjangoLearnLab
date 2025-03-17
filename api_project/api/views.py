9477af84bc724248a114ab2eeffe154eac073243
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-17 00:09:43 +0000

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from django_filters import rest_framework
 from django_filters import rest_framework as filters
 from rest_framework import viewsets, filters as drf_filters
+from django_filters import rest_framework as filters
 
 class BookList(generics.ListAPIView):
     queryset = book.objects.all()  
@@ -16,6 +17,27 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  
     serializer_class = BookSerializer  
     permission_classes = [IsAuthenticated] 
+
+class BookFilter(filters.FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains') 
+    author = filters.CharFilter(field_name='author__name', lookup_expr='icontains') 
+    publication_year = filters.NumberFilter() 
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [
+        filters.DjangoFilterBackend,  
+        drf_filters.SearchFilter,     
+        drf_filters.OrderingFilter    
+    ]
+    filterset_class = BookFilter
+
 
 class BookFilter(filters.FilterSet):
     title = filters.CharFilter(lookup_expr='icontains') 
