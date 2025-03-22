03f80da488530fc00ed6c0524ffb68c550b4ca3e
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-22 02:00:49 +0000

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
+from .models import Post, Tag
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -24,4 +25,17 @@ class CommentForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'})
         }
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
 
+    tags = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter tags separated by commas'})
+    )
+
+    def clean_tags(self):
+        tags = self.cleaned_data['tags']
+        tag_list = [tag.strip() for tag in tags.split(',') if tag.strip()]
+        return tag_list
