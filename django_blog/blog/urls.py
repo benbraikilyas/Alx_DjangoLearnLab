c15cca3a22b095ddc5366ffa62f8fd98fba5d99e
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-22 04:27:44 +0000

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -54,4 +54,10 @@ urlpatterns = [
     path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
 ]
 
+urlpatterns = [
+    path('search/', views.search_posts, name='search_posts'),
+    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
+]
+
+
 
