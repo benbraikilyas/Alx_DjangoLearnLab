8a9f9ec381a64733d4ed2518c676ae44776368f3
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-22 04:43:04 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager  # Import TaggableManager from taggit
 
 
 class Post(models.Model):
@@ -78,3 +79,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()  # Use TaggableManager for tags
+
+    def __str__(self):
+        return self.titl
