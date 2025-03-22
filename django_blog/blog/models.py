164bd821d919027e414a1039872ace621d3908af
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-03-22 00:08:03 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -24,3 +24,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
